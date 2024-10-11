8b2543410496f0984c234833d388f610a464a49c
Truphy <truphenaokenye@gmail.com>
2024-10-11 23:46:00 +0300

    'secondcommit'

--- config/config.php
+++ config/config.php
@@ -1,9 +1,15 @@
 
 <?php
-$servername = "localhost";
-$username = "root"; // Change as per your configuration
-$password = ""; // Change as per your configuration
-$dbname = "IKIGAI"; // Change to your database name
+require 'vendor/autoload.php';
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+
+$servername = $_ENV['servername'];
+$username = $_ENV['username'];
+$password = $_ENV['password'];
+$dbname = $_ENV['dbname'];
 
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
