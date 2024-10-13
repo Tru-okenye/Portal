4666481100dd3ac5eecc610f097bd0b1e4ed4ffd
Truphy <truphenaokenye@gmail.com>
2024-10-13 13:38:02 +0300

    wewe

--- login_form.php
+++ login_form.php
@@ -59,7 +59,7 @@ session_start();
             text-align: center; */
             display: flex;
             flex-direction: column;
-            /* justify-content: center; */
+            justify-content: center;
             align-items: center;
             background-color: white;
             border-radius: 5px;
